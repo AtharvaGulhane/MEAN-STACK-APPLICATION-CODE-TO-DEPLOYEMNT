6887f91752dff3d5f3d17e3392ebc0f461ff4db6
gulhaneatharva <gulhaneatharva@gmail.com>
2024-09-22 14:38:50 +0530

    MEAN STACK APPLICATION WORKING

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes'; // Update path as needed
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient()]
 };
